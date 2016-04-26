1f0f1587b74f292b4fece3f8d795a0eeb4371074
asvimalo <asvimalo@gmail.com>
2016-04-26 05:24:02 +0200

    Created tables/classes with attributes and PK/FK

---
+++ Real Estate/TypeOfContract.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace Real_Estate
+{
+    public class TypeOfContract
+    {
+        public int TypeOfContractID { get; set; }
+        public string Type { get; set; }
+
+        public double PercentualConstant { get; set; }
+    }
+}
