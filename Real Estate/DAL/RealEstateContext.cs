f4a9cd8fc6f6276b6f86d119d9b8b419e4aa6ee6
Kenneth Glawe <kenneth.glawe@gmail.com>
2016-04-26 10:06:46 +0200

    Added all models to context

--- Real Estate/DAL/RealEstateContext.cs
+++ Real Estate/DAL/RealEstateContext.cs
@@ -13,8 +13,11 @@ namespace Real_Estate.DAL
         public RealEstateContext() : base("RealEstateContext") { }
 
         public DbSet<Agent> Agents { get; set; }
+        public DbSet<City> Cities { get; set; }
         public DbSet<Customer> Customers { get; set; }
+        public DbSet<Office> Offices { get; set; }
         public DbSet<Person> Persons { get; set; }
+        public DbSet<RealEstateObject> RealEstateObjects { get; set; }
         public DbSet<Viewing> Viewings { get; set; }
         public DbSet<ZipCode> ZipCodes { get; set; }
 
