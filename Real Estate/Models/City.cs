c8f5edc603f1f5de85b74b1f48d6cde3f29b391b
Roppin.L <robin.ljungqvist@gmail.com>
2016-04-26 12:49:10 +0200

    Added ICollection<Person> to City class

--- Real Estate/Models/City.cs
+++ Real Estate/Models/City.cs
@@ -12,11 +12,13 @@ namespace Real_Estate.Models
         public City()
         {
             this.Offices = new HashSet<Office>();
+            this.Persons = new HashSet<Person>();
         }
 
         public int CityID { get; set; }
         public string CityName { get; set; }
 
         public virtual ICollection<Office> Offices { get; set; }
+        public virtual ICollection<Person> Persons { get; set; }
     }
 }
