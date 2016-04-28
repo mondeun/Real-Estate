af9833869fd1b17dfce81c4b6e8034caaf847f48
Kenneth Glawe <kenneth.glawe@gmail.com>
2016-04-28 12:27:58 +0200

    Fix for seed

--- Real Estate/Migrations/Configuration.cs
+++ Real Estate/Migrations/Configuration.cs
@@ -1,10 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Migrations;
+using System.Linq;
+using Real_Estate.Models;
+
 namespace Real_Estate.Migrations
 {
-    using System;
-    using System.Data.Entity;
-    using System.Data.Entity.Migrations;
-    using System.Linq;
-
     internal sealed class Configuration : DbMigrationsConfiguration<Real_Estate.DAL.RealEstateContext>
     {
         public Configuration()
@@ -15,18 +17,29 @@ namespace Real_Estate.Migrations
 
         protected override void Seed(Real_Estate.DAL.RealEstateContext context)
         {
-            //  This method will be called after migrating to the latest version.
+            context.Cities.AddOrUpdate(x => x.CityID,
+                new City {CityID = 1, CityName = "Helsingborg"},
+                new City {CityID = 2, CityName = "Lund"},
+                new City {CityID = 3, CityName = "Malmö"},
+                new City {CityID = 4, CityName = "Stockholm"},
+                new City {CityID = 5, CityName = "Göteborg"},
+                new City {CityID = 6, CityName = "Landskrona"},
+                new City {CityID = 7, CityName = "Ystad"}
+                );
 
-            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
-            //  to avoid creating duplicate seed data. E.g.
-            //
-            //    context.People.AddOrUpdate(
-            //      p => p.FullName,
-            //      new Person { FullName = "Andrew Peters" },
-            //      new Person { FullName = "Brice Lambson" },
-            //      new Person { FullName = "Rowan Miller" }
-            //    );
-            //
+            context.ZipCodes.AddOrUpdate(x => x.ZipCodeID,
+                new ZipCode {ZipCodeID = 1, ZipCodes = "621 41", PricePerSqm = 4500},
+                new ZipCode {ZipCodeID = 2, ZipCodes = "614 53", PricePerSqm = 8400},
+                new ZipCode {ZipCodeID = 3, ZipCodes = "342 15", PricePerSqm = 6700},
+                new ZipCode {ZipCodeID = 4, ZipCodes = "152 42", PricePerSqm = 3900},
+                new ZipCode {ZipCodeID = 5, ZipCodes = "423 21", PricePerSqm = 5500}
+                );
+
+            context.Offices.AddOrUpdate(x => x.OfficeID,
+                new Office {OfficeID = 1, OfficeName = "Goda Hem", Address = "Storgatan 4", ZipCodeID = 1, CityID = 6},
+                new Office {OfficeID = 2, OfficeName = "Sylen", Address = "Eriksväg 23", ZipCodeID = 2, CityID = 7},
+                new Office {OfficeID = 3, OfficeName = "Bosses Hörna", Address = "Leifgatan 31", ZipCodeID = 4, CityID = 2}
+                );
         }
     }
 }
