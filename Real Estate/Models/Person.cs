ff54d26a7256f08af2227b5766357b677f896576
Kenneth Glawe <kenneth.glawe@gmail.com>
2016-04-29 18:06:06 +0200

    Added validation for the person class

--- Real Estate/Models/Person.cs
+++ Real Estate/Models/Person.cs
@@ -23,10 +23,20 @@ namespace Real_Estate.Models
         public virtual City City { get; set; }  //TODO City is empty - Andres does
 
         // Other attributes
+        [Required(ErrorMessage = "First name is required")]
         public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Last name is required")]
         public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Personal number is required")]
         public string Pnr { get; set; }
+
+        [Required(ErrorMessage = "Address is required")]
         public string Address { get; set; }
+
+        [Required(ErrorMessage = "Email address is required")]
+        [EmailAddress(ErrorMessage = "Invalid Email Address")]
         public string Email { get; set; }
 
         public string FullName
