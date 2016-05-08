5b26e324e3a767ef0c2b9a89c8782404aff6032d
Roppin.L <robin.ljungqvist@gmail.com>
2016-05-08 18:49:24 +0200

    Added some viewings and new customers to the seed.

--- Real Estate/Migrations/Configuration.cs
+++ Real Estate/Migrations/Configuration.cs
@@ -239,8 +239,8 @@ namespace Real_Estate.Migrations
                     ViewingID = 5,
                     RealEstateObjectID = 3,
                     AgentID = 2,
-                    ViewingStart = new DateTime(2015, 12, 05, 15, 30, 0),
-                    ViewingStop = new DateTime(2015, 12, 05, 18, 0, 0),
+                    ViewingStart = new DateTime(2016, 01, 05, 15, 30, 0),
+                    ViewingStop = new DateTime(2016, 01, 05, 18, 0, 0),
                     MaximumNoCustomerPerViewing = 10
                 },
                 new Viewing
@@ -248,8 +248,8 @@ namespace Real_Estate.Migrations
                     ViewingID = 6,
                     RealEstateObjectID = 3,
                     AgentID = 2,
-                    ViewingStart = new DateTime(2015, 12, 05, 13, 0, 0),
-                    ViewingStop = new DateTime(2015, 12, 05, 15, 0, 0),
+                    ViewingStart = new DateTime(2016, 01, 05, 13, 0, 0),
+                    ViewingStop = new DateTime(2016, 01, 05, 15, 0, 0),
                     MaximumNoCustomerPerViewing = 10
                 }
                 );
